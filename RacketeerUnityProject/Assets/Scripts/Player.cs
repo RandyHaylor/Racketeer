5bcb94590c0e13c99b875db075f204c895237dcb
Daniel Benson <dbenson24@gmail.com>
2021-04-10 18:56:37 -0500

    updated player script and player prefab

--- RacketeerUnityProject/Assets/Scripts/Player.cs
+++ RacketeerUnityProject/Assets/Scripts/Player.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-    public float movementSpeed = 10;
-    public float rotationSpeed = 10;
-    float moveHorizontal;
-    float horizontalMovementMult = 25f;
-    float moveVertical;
-    float verticalMovementMult = 25f;
-    float rotate;
-    float rotateMult = -10f;
-    Vector3 movement;
-    Rigidbody rb;
+    public float rotateMult = -10f;
+    public float verticalMovementMult = 25f;
+    public float horizontalMovementMult = 25f;
+
+    private float moveHorizontal;
+    private float moveVertical;
+    private float rotate;
+
+    private Vector3 movement;
+    private Rigidbody rb;
+
     private void Awake()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
     }
+
     void HandleMovement()
     {
         if (isLocalPlayer)
         {
             moveHorizontal = Input.GetAxis("Horizontal");
             moveVertical = Input.GetAxis("Vertical");
-            movement = new Vector3(moveHorizontal * horizontalMovementMult * Time.deltaTime * movementSpeed, moveVertical* verticalMovementMult * Time.deltaTime * movementSpeed, 0);
+            movement = new Vector3(moveHorizontal * horizontalMovementMult * Time.deltaTime, moveVertical * verticalMovementMult * Time.deltaTime, 0);
             //transform.position = transform.position + movement;
             rb.AddForce(movement);
             rotate = rotateMult * Input.GetAxis("RightStickHorizontal");
-            rb.AddTorque(transform.forward * rotate * Time.deltaTime * rotationSpeed);
+            rb.AddTorque(transform.forward * rotate * Time.deltaTime);
         }
     }
     void Update()
